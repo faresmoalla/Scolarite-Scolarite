41c04aa043ee03587f1b09e1b7d512e8b7939627
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2023-02-28 22:26:40 +0100

    project

---
+++ src/app/employe/employe.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeRoutingModule } from './employe-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployeRoutingModule
+  ]
+})
+export class EmployeModule { }
---
+++ src/app/etudiant/etudiant.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EtudiantRoutingModule } from './etudiant-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EtudiantRoutingModule
+  ]
+})
+export class EtudiantModule { }
