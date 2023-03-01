82b11e28d5a9d57c7714d13fe90d96956904f581
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2023-03-01 15:50:31 +0100

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
