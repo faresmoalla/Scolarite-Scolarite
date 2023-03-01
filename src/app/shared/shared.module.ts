c0171562e723648229ffb4483f614ea6fd7b59fe
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2023-03-01 15:26:00 +0100

    nv project

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedRoutingModule } from './shared-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedRoutingModule
   ]
 })
 export class SharedModule { }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ]
